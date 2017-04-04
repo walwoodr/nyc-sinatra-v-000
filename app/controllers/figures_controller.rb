881c4becf0ee822ef60cabdab91b3456d8b3684f
walwoodr <walwoodr@gmail.com>
2017-04-04 10:25:38 -0500

    Outline of controller and views for figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,27 @@
 class FiguresController < Sinatra::Base
 
   get '/figures' do
-    erb :'/figures/index'
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    erb :'figures/show'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    # create new figure
+  end
+
+  get '/figures/:id/edit' do
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    # modify existing figure
   end
 
 end
