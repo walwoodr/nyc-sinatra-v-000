818a120f05f7c5fb3efd28ce34351cdf7ad668b6
walwoodr <walwoodr@gmail.com>
2017-04-04 11:15:24 -0500

    Add index view

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,16 +1,16 @@
-class FiguresController < Sinatra::Base
+class FiguresController < ApplicationController
 
   get '/figures' do
     erb :'figures/index'
   end
 
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
-  end
-
-  get '/figures/new' do
-    erb :'figures/new'
   end
 
   post '/figures' do
