fabbd5f8c558814b7a2e9d826d9de5f07ab221e7
walwoodr <walwoodr@gmail.com>
2017-04-04 10:39:43 -0500

    Index page and create new landmark page

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmarks.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    #create new landmark
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmarks.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    # modify existing landmark
+  end
+
+  # creates checkboxes for all the landmarks and titles created on teh Figures new page. ???
+
 end
