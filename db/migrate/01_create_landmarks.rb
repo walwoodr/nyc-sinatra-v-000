7318f0c43d6f0e90ac35998c21618af8a6d87de1
walwoodr <walwoodr@gmail.com>
2017-04-04 09:29:50 -0500

    Modify landmark migration

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,9 +1,11 @@
 class CreateLandmarks < ActiveRecord::Migration
 
-  create_table :landmarks do |t|
-    t.string :name
-    t.integer :figure_id
-    t.string :year_completed
+  def change
+      create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.string :year_completed
+    end
   end
-
+  
 end
